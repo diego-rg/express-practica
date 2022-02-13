d21d4547a9d6dd4e705a26ef64c5c33372db8dc3
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-02-13 22:00:25 +0100

    using pug for templating

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import path from "path";//Para configurar o path das views para pug
 import express from "express";
 import morgan from "morgan";//HTTP request logger middleware. Fai logs das request e da info delas
 import bodyParser from "body-parser";//middleware que cando recibe unha petición parsea o que recibe e gárdao no body da request
@@ -8,10 +9,15 @@ app.disable("x-powered-by");//Desactiva nos headers da response a info de que a 
 
 app.set("env", "development");//Indicalle a express que estamos en desarrollo, non en producción
 
-app.use(morgan());
+app.use(morgan("combined"));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set("views", path.join(__dirname, "views"));//Indica que as views quevamos a usar estarán no directorio actual/views
+app.set("view engine", pug);//indicamos que usaremos pug como motor de plantillas
 
 app.get("/", (req, res) => {
-    res.send("Boas!");
+    res.render("home");
 });
 
 app.listen("3000", () => {
