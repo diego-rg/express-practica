1d32d0dd9641fd0f87d678d57172caa71c87df0d
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-02-13 22:20:08 +0100

    pug variables, __dirname fix for es6 modules

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import path from "path";//Para configurar o path das views para pug
-import express from "express";
+import express from "express";//Express será o que transforma o cñodigo da plantilla de pug a html para o navegador
 import morgan from "morgan";//HTTP request logger middleware. Fai logs das request e da info delas
 import bodyParser from "body-parser";//middleware que cando recibe unha petición parsea o que recibe e gárdao no body da request
+
+//Fix para que funcione __dirname con "type": "module" de package.json
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -9,15 +15,18 @@ app.disable("x-powered-by");//Desactiva nos headers da response a info de que a 
 
 app.set("env", "development");//Indicalle a express que estamos en desarrollo, non en producción
 
-app.use(morgan("combined"));
+app.use(morgan("tiny"));//configuramos que info nos manda morgan: combined, tiny, etc por cada request que nos mandan
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set("views", path.join(__dirname, "views"));//Indica que as views quevamos a usar estarán no directorio actual/views
-app.set("view engine", pug);//indicamos que usaremos pug como motor de plantillas
+app.set("view engine", "pug");//indicamos que usaremos pug como motor de plantillas
 
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render("home",  {
+        createdBy: "unha variable",
+        creationDate: "un día destes"
+    });
 });
 
 app.listen("3000", () => {
